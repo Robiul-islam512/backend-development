7f7e1608f35d080d877dfde57f48af45d307c115
Robiul-islam512 <robiulislam7854@gmail.com>
2025-09-05 01:48:50 +0600

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// console.log(process.env.MONGODB_URI);
+const connectDB = async ()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n Mongo DB Connected !! DB Host: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONOGODB connection error: ",error);
+        process.exit(1);
+    }
+    
+}
+export default connectDB;
